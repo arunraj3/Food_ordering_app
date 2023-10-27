5aff064637ebca6b39b1f2c783d3d8150fc60b46
arunraj3 <arunrajgovindan2002@gmail.com>
2023-10-27 22:52:47 +0530

    Basic Header Component

--- App.js
+++ App.js
@@ -1,1 +1,65 @@
-import React from "react"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+import logo from "./AR_foodie_logo.png";
+/*
+ Reference model 
+
+ MainComponent 
+    Header
+        Logo
+        Navigation Items
+    Body
+        Search
+        RestaurantContainer
+            RestaurantCard
+    Footer
+        Copyright
+        Links
+        Address
+        Contact
+*/
+
+const Header = () => {
+  return (
+    <div className="header">
+      {/*  
+            logo 
+            Nav-items 
+     */}
+
+      <img src={logo} className="logo"/>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contacts</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body =()=>{
+    return(
+        <div className="body-component">
+            <div className="Restaurant-container">
+                
+            </div>
+        </div>
+    );
+}
+const App = () => {
+  return (
+    <div className="app-layout">
+      {/* 
+        Header
+        Body
+        Footer
+         */}
+      <Header />
+    </div>
+  );
+};
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
