8a1cda92723429afb6abbbc8399d5e6182230401
arunraj3 <arunrajgovindan2002@gmail.com>
2024-06-01 16:08:04 +0530

    Added Images

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./AR_foodie_logo.jpg"
+
+import "./styles.css"
 /* 
  App
   - Header
@@ -16,11 +19,55 @@ import ReactDOM from "react-dom/client";
     -Contact
 */
 
-const App = () => {
+const HeaderComponent = () => {
   return (
     <>
-      <h1>Sample Testing Component</h1>
+      <div className="header">
+        <div className="logo-container">
+          <img className ="logo" src={logo}></img>
+        </div>
+        <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+      </div>
     </>
   );
 };
+
+
+
+const RestaurantCard =({})=>{
+  return(
+  <div className="restaurant-card">
+    <img className="restaurant-logo" src={{}} alt="restaurant-logo"></img>
+    <div>Restaurant-name</div>
+    <div>Ratings</div>
+    <div>Location</div>
+  </div>);
+}
+
+const BodyComponent =()=>{
+  return(
+    <div className="body-component">
+      <div className="search">Search</div>
+      <div className="restaurant-container">
+        <RestaurantCard/>
+      </div>
+    </div>
+  )
+}
+const App = () => {
+  return (
+  <>
+    <HeaderComponent/>
+    <BodyComponent/>
+  </>);
+};
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
